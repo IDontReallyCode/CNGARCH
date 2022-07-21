fe1011ba73f31e80ce1c1ba9102f64986be1c7a3
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2022-07-21 09:55:10 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='CNGARCH',
+version='0.1',
+description='Estimate and Forecast using Component NGARCH models',
+url='#',
+author='max',
+author_email='letournp@uww.edu',
+packages=setuptools.find_packages(),
+zip_safe=False)
